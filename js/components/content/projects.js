4516787c75b37a33e51ce1176da59e717477b207
mei <yimei.wym@gmail.com>
2020-10-26 21:42:35 +0800

    inject project programmatically, some stylistic changes

---
+++ js/components/content/projects.js
@@ -0,0 +1,87 @@
+const PROJECTS = [{
+        "id": 0,
+        "caption": "SAT vs ACT Analysis",
+        "subcaption": "Comparison of SAT and ACT scores between years 2017 & 2018",
+        "tech": [
+            "Matplotlib",
+            "Seaborn",
+            "Pandas",
+            "Numpy"
+        ],
+        "descriptions": [
+            "Used the analysis performed to make recommendations to improve SAT participation of states with lower rates",
+            "placeholder for description2\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+            "placeholder for description3\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+        ],
+        "githubUrl": "https://github.com/fadhilaffandi/dsiprojects/tree/master/project_1",
+        "imageUrl": "img/team/2.jpg"
+    },
+    {
+        "id": 1,
+        "caption": "Ames Housing Predictions",
+        "subcaption": "subcaption!",
+        "tech": [],
+        "descriptions": [
+            "placeholder for description1\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+            "placeholder for description2\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+            "placeholder for description3\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+        ],
+        "githubUrl": "www.github.com",
+        "imageUrl": "img/team/2.jpg"
+    },
+    {
+        "id": 2,
+        "caption": "SubReddit Classifier",
+        "subcaption": "subcaption!",
+        "tech": [],
+        "descriptions": [
+            "placeholder for description1\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+            "placeholder for description2\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+            "placeholder for description3\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+        ],
+        "githubUrl": "www.github.com",
+        "imageUrl": "img/team/2.jpg"
+    },
+    {
+        "id": 3,
+        "caption": "West Nile Virus Predictions",
+        "subcaption": "subcaption!",
+        "tech": [],
+        "descriptions": [
+            "placeholder for description1\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+            "placeholder for description2\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+            "placeholder for description3\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis ipsam! Ipsam voluptatibus odit consequuntur.",
+        ],
+        "githubUrl": "www.github.com",
+        "imageUrl": "img/team/2.jpg"
+    },
+    {
+        "id": 4,
+        "caption": "Sydney Daily Rainfall Predictions",
+        "subcaption": "Prediction of daily rainfall for the city of Sydney to aid the Department of Planning, Industry and Environment in planning the city's infrastructure",
+        // "tech": [
+        //     "Matplotlib",
+        //     "Seaborn",
+        //     "Scikit-Learn",
+        //     "Pandas",
+        //     "Numpy",
+        //     "Tensorflow"
+        // ],
+        "tech": {
+            "Model(s) utilised": ["I tak tau the model"],
+            "Visualisation": ["Matplotlib", "Seaborn"],
+            "Packages": ["Scikit-Learn", "Pandas", "Numpy"]
+        },
+        "descriptions": [
+            "Data set of 18 columns of weather features and about 3000 rows of data",
+            "Predicting the amount of Rainfall in ppt per day",
+            "Predictions generated through comparing SARIMAX, Fb Prophet and Gated Recurrent Units",
+            "Mean Squared Error of 89mm",
+            "Completed with Python and Jupyter Notebook"
+        ],
+        "githubUrl": "https://github.com/fadhilaffandi/dsiprojects/tree/master/Sydney%20Daily%20Rainfall%20Predictions",
+        "imageUrl": "img/team/2.jpg"
+    },
+]
+
+export { PROJECTS };
---
+++ js/components/projects.js
@@ -0,0 +1,103 @@
+import { PROJECTS } from "./content/projects.js";
+
+
+Object.entries(PROJECTS).reverse().map(([key, item]) => {
+    console.log(item);
+
+    // create thumbnail
+    let thumbnailWrapper = buildImage(item.imageUrl);
+
+    // create project description
+    let descWrapper = buildDesc(item);
+
+    // bundle thumbnail and description together
+    let outermostWrapper = buildElementWithClass("row portfolio-item p-0 my-5", "section");
+
+    outermostWrapper.appendChild(thumbnailWrapper);
+    outermostWrapper.appendChild(descWrapper);
+
+    document.querySelector("#projects .content-wrapper")
+        .appendChild(outermostWrapper);
+})
+
+
+function buildImage(imageUrl) {
+
+    let projectImg = buildElementWithClass("img-fluid m-3", "img");
+    projectImg.setAttribute("src", imageUrl);
+
+    let thumbnailWrapper = buildElementWithClass("col-lg-3 col-12 project-image d-flex justify-content-center");
+    thumbnailWrapper.appendChild(projectImg);
+
+    return thumbnailWrapper;
+}
+
+
+function buildDesc(project) {
+
+    let captionContainer = buildCaption(project.caption, project.subcaption, project.githubUrl);
+
+    // append descriptions statements to project
+    let techSubheading = buildElementWithClass("", "h5");
+    techSubheading.innerHTML = "Technologies used";
+    let tech = buildList(project.tech);
+
+    let descSubheading = buildElementWithClass("", "h5");
+    descSubheading.innerHTML = "Description";
+    let description = buildList(project.descriptions);
+
+    let descContainer = buildElementWithClass("portfolio-content");
+
+    descContainer.appendChild(techSubheading);
+    descContainer.appendChild(tech);
+
+    descContainer.appendChild(descSubheading);
+    descContainer.appendChild(description);
+
+
+    let descWrapper = buildElementWithClass("col-lg-8 col-12 pl-0");
+    descWrapper.appendChild(captionContainer);
+    descWrapper.appendChild(descContainer);
+
+    return descWrapper;
+}
+
+
+function buildCaption(caption, subcaption, githubUrl) {
+
+    let captionHeading = buildElementWithClass("portfolio-caption-heading");
+    captionHeading.innerHTML = caption;
+
+    let subheading = buildElementWithClass("portfolio-caption-subheading text-muted")
+    subheading.innerHTML = subcaption;
+
+    let githubLink = buildElementWithClass("small", "a");
+    githubLink.setAttribute("href", githubUrl);
+    githubLink.innerHTML = "Click to see this on GitHub";
+
+    let captionContainer = buildElementWithClass(["portfolio-caption"]);
+    captionContainer.appendChild(captionHeading);
+    captionContainer.appendChild(subheading);
+    captionContainer.appendChild(githubLink);
+
+    return captionContainer;
+}
+
+
+function buildList(content) {
+    let ul = buildElementWithClass("", "ul");
+    Object.entries(content).forEach(([key, desc]) => {
+        let li = buildElementWithClass("", "li");
+        li.innerHTML = desc;
+        ul.appendChild(li);
+    });
+
+    return ul;
+}
+
+function buildElementWithClass(classlist, element = "div") {
+    let container = document.createElement(element);
+    container.setAttribute("class", classlist);
+
+    return container;
+}
